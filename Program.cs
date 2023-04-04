8611885c0a425b2c77aa6df9a3b420e6024c1237
Lenar Minikaev <minikaev_lenar@.mail.ru>
2023-04-04 12:13:53 +0300

    Creating a block for filling a request for values of an array of strings

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string [] fill_array(string [] array){
+    int size_array = array.Length;
+    for (int i = 0; i < size_array; i++){
+        Console.Write($"Введите {i + 1} из {size_array} эллемент >> ");
+        array[i] = Console.ReadLine();
+    }
+    return array;
+}
+
+
+
+Console.Write("Введите размер массива >> ");
+int array_size = Convert.ToInt32(Console.ReadLine());
+string [] my_array = new string[array_size];
+my_array = fill_array(my_array);
