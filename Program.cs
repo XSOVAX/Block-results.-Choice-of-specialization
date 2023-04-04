623d717b70238ca20a552d33e97917aaeb4ce9a5
MLR <lenar_sova87@mail.ru>
2023-04-04 14:19:20 +0300

    added function to output an array of strings

--- Program.cs
+++ Program.cs
@@ -7,9 +7,20 @@
     return array;
 }
 
-
+void print_array(string[] array){
+    int size_array = array.Length;
+    Console.Write("[");
+    if(size_array > 0){
+        Console.Write($"{array[0]}");
+        for(int i = 1; i < size_array; i++){
+            Console.Write($"; {array[i]}");
+        }
+    }
+    Console.WriteLine("]");
+}
 
 Console.Write("Введите размер массива >> ");
 int array_size = Convert.ToInt32(Console.ReadLine());
-string [] my_array = new string[array_size];
-my_array = fill_array(my_array);
+string [] data_array = new string[array_size];
+data_array = fill_array(data_array);
+print_array(data_array);
