870f26583a4e5ecf19e826371ce945ea31331f22
MLR <lenar_sova87@mail.ru>
2023-04-06 11:01:10 +0300

    fixing the proccessing_array function fixed the solution for a dynamic array

--- Program.cs
+++ Program.cs
@@ -9,13 +9,13 @@ string [] fill_array(int size_array){
 
 string [] array_proccessing(string [] array){
     int size_array = array.Length;
-    string[] proccessing_result = new string[size_array];
-    int index_proccessin_array = 0;
+    int size_array_proccessing = size_array;
+    string[] proccessing_result = new string[] {};
     for(int i = 0; i < size_array; i++){
         string element_array = array[i];
         if(element_array.Length < 4){
-            proccessing_result[index_proccessin_array] = element_array;
-            index_proccessin_array++;
+            Array.Resize(ref proccessing_result, proccessing_result.Length + 1);
+            proccessing_result[proccessing_result.Length - 1] = element_array;
         }
     }
     return proccessing_result;
@@ -27,7 +27,7 @@ void print_array(string[] array){
     if(size_array > 0){
         Console.Write($"{array[0]}");
         for(int i = 1; i < size_array; i++){
-            Console.Write($"; {array[i]}");
+                Console.Write($"; {array[i]}");
         }
     }
     Console.WriteLine("]");
